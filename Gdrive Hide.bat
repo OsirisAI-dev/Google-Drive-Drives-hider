5f182099d34e3613e71b85a127a97b193e2fbac7
Osiris <300155853+OsirisAI-dev@users.noreply.github.com>
2026-07-06 01:32:09 +0000

    Delete Gdrive Hide.bat

--- Gdrive Hide.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0DriveManager.ps1""' -Verb RunAs}"
